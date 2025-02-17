e2b113d3fbbc92e1f7cac290b3eedf6e4c1fb551
Luis R <78448904+Starmise@users.noreply.github.com>
2025-02-16 21:31:17 -0600

    UI upgrated
    - Added a command to go back to MainMenu in every minigame
    - Modified the MainManu Canvas Scale method
    - Added the correspondant scenes to the buttons parameters in Main Menu

--- BoneVoyage/Assets/Scripts/SceneChanger.cs
+++ BoneVoyage/Assets/Scripts/SceneChanger.cs
@@ -3,6 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    // Nomás que se reinicie el juego (se manda al menú) si s epone M
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
